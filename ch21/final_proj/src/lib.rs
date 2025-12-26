ca463c9709709567da22251e19fa0b77207f9e74
ffengjay <2652925120@qq.com>
2025-12-26 14:55:01 +0800

    [update] final multi-threads server of Chapter21

---
+++ ch21/final_proj/src/lib.rs
@@ -0,0 +1,73 @@
+use std::thread;
+use std::sync::{mpsc, Arc, Mutex};
+
+pub struct ThreadPool{
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool{
+        assert!(size > 0);
+        let (sx, rx) = mpsc::channel();
+        let mut workers = Vec::with_capacity(size);
+        let receiver = Arc::new(Mutex::new(rx));
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+        ThreadPool{workers, sender: Some(sx)}
+    }
+
+    pub fn execute<F>(&self, _f: F)
+        where F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(_f);
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        for worker in self.workers.drain(..){
+            println!("Shutting down worker {}", worker.id);
+            worker.thread.join().unwrap();
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let message = receiver.lock().unwrap().recv();
+                match message {
+                    Ok(job) => {
+                        println!("Worker {id} got a job; executing.");
+                        job();
+                    },
+                    Err(_) => {
+                        println!("Worker {id} disconnected; shutting down.");
+                        break;
+                    }
+                }
+            }
+        });
+        Worker { id, thread }
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
